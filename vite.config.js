edf9744af5c19b775af35ec6af9a6e357daf4d1a
Mohamed Aziz Rezgui <rezguiaziz32@gmail.com>
2025-12-25 00:50:10 +0100

    Save work before deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/Portfolio_Mohamed_aziz_rezgui/', // replace REPO_NAME with your GitHub repo name
+  base: './', // use relative paths for GitHub Pages
 
 })
